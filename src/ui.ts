49969d55a362e8f9fb309bb6aead0ad2854b9804
jwellnit <mawellni@ncsu.edu>
2021-04-01 19:13:13 -0400

    The buildings are moving?

--- src/ui.ts
+++ src/ui.ts
@@ -2,8 +2,10 @@ import * as npc from "./agent";
 import * as main from "./main";
 import * as exec from "./execution_engine";
 
-var n:number = 8;
+var n:number = 6;
+var sleep:number = 10;
 var board:string[][] = [];
+
 for(var i:number=0; i<n; i++){
 	board[i]=[];
 	for(var j:number=0; j<n; j++){
@@ -11,11 +13,29 @@ for(var i:number=0; i<n; i++){
 	}
 }
 
+function clearBoard() {
+	for(var i:number=0; i<n; i++){
+		for(var j:number=0; j<n; j++){
+			board[i][j] = "";
+		}
+	}
+}
+
 //Executed every turn, calls the next turn if applicable
 export function updateUI(agentList:npc.Agent[], actionList:npc.Action[], locationList:npc.Location[], continueFunction: () => boolean, time:number){
   main.showOnBrowser("time", time.toString());
+	clearBoard();
+	for (let l of locationList){
+		board[l.xPos][l.yPos] += l.name[0];
+	}
+	for(var i:number=0; i<n; i++){
+		for(var j:number=0; j<n; j++){
+			var div:string = "space " + i.toString() + "-" + j.toString();
+			main.showOnBrowser(div, board[i][j]);
+		}
+	}
 	if (continueFunction()) {
-		setTimeout(() => {exec.run_sim(agentList, actionList, locationList, continueFunction)}, 10);
+		setTimeout(() => {exec.run_sim(agentList, actionList, locationList, continueFunction)}, sleep);
 	} else {
 		console.log("Finished.");
 	}
